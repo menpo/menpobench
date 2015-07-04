67805747e9e38df873b508c878aa1c42f48b7a76
James Booth <jab08@narsil.lan>
2015-07-04 11:21:28 +0100

    add pathlib as a dependency

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
+import sys
 import versioneer
 
+install_requires = ['menpofit>=0.2,<0.3']
+
+if sys.version_info.major == 2:
+      install_requires.append('pathlib==1.0')
 
 setup(name='menpobench',
       version=versioneer.get_version(),
@@ -10,4 +15,4 @@ setup(name='menpobench',
       author_email='james.booth08@imperial.ac.uk',
       packages=find_packages(),
       tests_require=['nose'],
-      install_requires=['menpofit>=0.2,<0.3'])
+      install_requires=install_requires)
