a6e524f19bfe6375e7281d6025daaf7654fc549b
Patrick Snape <patricksnape@gmail.com>
2015-07-09 22:19:16 +0100

    More work on yorgos' code
    At the moment all the matlab functions are lazily inline, but
    i'm going to bed now so they have to stay there. Biggest issue
    is that matlab isn't on the path by default on OSX or
    Windows - so we need a matlab finding function.

---
+++ menpobench/predefined/method/yzt_iccv_2013.m
@@ -0,0 +1,3 @@
+function menpobench_train( images_path )
+
+end
--- menpobench/predefined/method/yzt_iccv_2013.py
+++ menpobench/predefined/method/yzt_iccv_2013.py
@@ -1,29 +1,92 @@
+import subprocess
+import numpy as np
+import shutil
+import os
+from menpo.visualize import print_progress
+from pathlib import Path
+import menpo.io as mio
 from menpobench.method.managed import managed_method
-from menpofit.aam import AAMBuilder, LucasKanadeAAMFitter
-from menpo.feature import dsift
-from menpobench.method import MenpoFitterWrapper
+from menpobench.method.base import matlab_functions_dir, predefined_method_dir
+from menpobench.base import TempDirectory
 from scipy.io import savemat, loadmat
 
 
 def images_to_mat(images, out_path):
-    image_tuples = [{'pixels': im.pixels,
-                     'gt_shape': im.landmarks['gt_shape'].lms.points,
-                     'bbox': im.landmarks['bbox'].lms.points}
+    as_fortran = np.asfortranarray
+    image_tuples = [{'pixels': as_fortran(im.rolled_channels()),
+                     'gt_shape': as_fortran(im.landmarks['gt_shape'].lms.points),
+                     # TODO: use bbox
+                     'bbox': as_fortran(im.landmarks['gt_shape'].lms.points)}
                     for im in images]
-    savemat(out_path, image_tuples)
+
+    mat_out_path = out_path / 'menpobench_training_images.mat'
+    print('Serializing training data to Matlab file: {}'.format(mat_out_path))
+    savemat(str(mat_out_path), {'menpobench_training_images': image_tuples})
+
+
+def save_images_to_dir(images, out_path, output_ext='.jpg'):
+    if not out_path.exists():
+        out_path.mkdir()
+    for k, im in enumerate(print_progress(images)):
+        mio.export_image(im, out_path / '{}{}'.format(k, output_ext))
+
+
+def save_landmarks_to_dir(images, label, out_path, output_ext='.pts'):
+    if not out_path.exists():
+        out_path.mkdir()
+    for k, im in enumerate(print_progress(images)):
+        mio.export_landmark_file(im.landmarks[label],
+                                 out_path / '{}{}'.format(k, output_ext))
+
+
+
+class MatlabWrapper(object):
+
+    def __init__(self, fitter):
+        self.fitter = fitter
+
+    def __call__(self, img_generator):
+        results = []
+        # for img in img_generator:
+        #     img = menpo_preprocess(img)
+        #     # obtain ground truth (original) landmarks
+        #     gt_shape = img.landmarks['gt_shape'].lms
+        #     results.append(self.fitter.fit(img, gt_shape, gt_shape=gt_shape))
+        return results
+
+
+def invoke_process(command_list):
+    print(' '.join(command_list))
+    subprocess.check_call(command_list)
+
+
+def invoke_matlab(command):
+    invoke_process(['matlab', '-wait', '-nosplash', '-nodesktop', '-nojvm',
+                    "'{}'".format(command)])
+
+
+def train_matlab_method(method_path, matlab_train_filename):
+    # Get absolute path to the train method and copy across to the metho
+    # folder
+    matlab_train_path = predefined_method_dir() / matlab_train_filename
+    shutil.copyfile(str(matlab_train_path),
+                    str(method_path / 'menpobench_train.m'))
+
+    # Call matlab bridge to train file - will drop out a model file
+    invoke_matlab('addpath({}); menpobench_matlab_train({});'.format(
+        matlab_functions_dir(), method_path))
+
 
 def train(img_generator):
-    with managed_method('yzt_iccv_2013') as method_path:
-        # whack images into mat file
+    with managed_method('yzt_iccv_2013', cleanup=False) as method_path:
+        train_path = method_path / 'menpobench_train_images'
         images = list(img_generator)
-        images_to_mat(images, method_path)
 
-        # build the AAM
-        aam = AAMBuilder(features=dsift,
-                         normalization_diagonal=120,
-                         n_levels=3).build(images, verbose=True)
-        fitter = LucasKanadeAAMFitter(aam, n_shape=0.9, n_appearance=0.9)
+        save_images_to_dir(images, train_path)
+        save_landmarks_to_dir(images, 'gt_shape', train_path)
 
-        # return a callable that wraps the menpo fitter in order to integrate
+        train_matlab_method(method_path, 'yzt_iccv_2013.m')
+
+        # return a callable that wraps the matlab fitter in order to integrate
         # with menpobench
-        return MenpoFitterWrapper(fitter)
+        return MatlabWrapper(None)
