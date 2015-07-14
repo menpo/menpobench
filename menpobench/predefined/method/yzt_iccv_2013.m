6916681719d6d15c70f69ec1ba73776de791673c
Patrick Snape <patricksnape@gmail.com>
2015-07-14 16:21:03 +0100

    Add fitting for Yorgos' method
    Also, fix up how n_iterations per scale is passed through
    by the model.

--- menpobench/predefined/method/yzt_iccv_2013.m
+++ menpobench/predefined/method/yzt_iccv_2013.m
@@ -48,20 +48,22 @@ function model_data = train(training_images_path)
     %% Truncate the AAM
     % This pre-computes a lot of data that makes fitting more efficient
     % Parameters taken from main_AAM
-    cAAM.shape{1}.n = 10;
-    cAAM.shape{2}.n = 3;
+    cAAM.shape{1}.n = min(10, length(pts_file_paths));
+    cAAM.shape{2}.n = min(3, length(pts_file_paths));
     cAAM.shape{1}.num_of_similarity_eigs = 4;
     cAAM.shape{2}.num_of_similarity_eigs = 4;
     cAAM.shape{1}.n_all = cAAM.shape{1}.n + cAAM.shape{1}.num_of_similarity_eigs;
     cAAM.shape{2}.n_all = cAAM.shape{2}.n + cAAM.shape{2}.num_of_similarity_eigs;
-    cAAM.texture{1}.m = 200;
-    cAAM.texture{2}.m = 50;
+    cAAM.texture{1}.m = min(200, length(pts_file_paths));
+    cAAM.texture{2}.m = min(50, length(pts_file_paths));
     
     cAAM.num_of_points = AAM.num_of_points;
     cAAM.scales = AAM.scales;
     cAAM.coord_frame = AAM.coord_frame;
     
     for ii = 1:num_of_scales
+        % MENPOBENCH: This is a new line added to store the number of iterations per scale.
+        cAAM.num_of_iter{ii} = 50;
         % shape
         cAAM.shape{ii}.s0 = AAM.shape.s0;
         cAAM.shape{ii}.S = AAM.shape.S(:, 1:cAAM.shape{ii}.n);
@@ -82,11 +84,77 @@ function model_data = train(training_images_path)
 end
 
 %% Setup
-function model_data = setup(method_path)
-    model_data = load(fullfile(method_path, 'model.mat'));
+function model = setup(method_path)
+    load(fullfile(method_path, 'model.mat'));
 end
 
 %% Fit
 function result = fit(image_data, bbox, model_data)
-    result = [];
+    cAAM = model_data;
+
+    %% initialization
+    s0 = cAAM.shape{1}.s0;
+    current_shape = menpobench_initial_shape_to_bbox(bbox, s0);
+    num_of_scales_used = length(cAAM.scales);
+
+    %% Fitting an AAM using Fast-SIC algorithm
+    sc = 2 .^ (cAAM.scales-1);
+    for ii = num_of_scales_used:-1:1
+        current_shape = current_shape / sc(ii);
+
+        % indices for masking pixels out
+        ind_in = cAAM.coord_frame{ii}.ind_in;
+        ind_out = cAAM.coord_frame{ii}.ind_out;
+        ind_in2 = cAAM.coord_frame{ii}.ind_in2;
+        ind_out2 = cAAM.coord_frame{ii}.ind_out2;
+        resolution = cAAM.coord_frame{ii}.resolution;
+
+        A0 = cAAM.texture{ii}.A0;
+        A = cAAM.texture{ii}.A;
+        AA0 = cAAM.texture{ii}.AA0;
+        AA = cAAM.texture{ii}.AA;
+
+        for i = 1:cAAM.num_of_iter{ii}
+
+            % Warp image
+            Iw = warp_image(cAAM.coord_frame{ii}, current_shape*sc(ii), image_data);
+            I = Iw(:); I(ind_out) = [];
+            II = Iw(:); II(ind_out2) = [];
+
+            % compute reconstruction Irec
+            if (i == 1)
+                c = A'*(I - A0) ;
+            else
+                c = c + dc;
+            end
+            Irec = zeros(resolution(1), resolution(2));
+            Irec(ind_in) = A0 + A*c;
+
+            % compute gradients of Irec
+            [Irecx Irecy] = gradient(Irec);
+            Irecx(ind_out2) = 0; Irecy(ind_out2) = 0;
+            Irec(ind_out2) = [];
+            Irec = Irec(:);
+
+            % compute J from the gradients of Irec
+            J = image_jacobian(Irecx, Irecy, cAAM.texture{ii}.dW_dp, cAAM.shape{ii}.n_all);
+            J(ind_out2, :) = [];
+
+            % compute Jfsic and Hfsic
+            Jfsic = J - AA*(AA'*J);
+            Hfsic = Jfsic' * Jfsic;
+            inv_Hfsic = inv(Hfsic);
+
+            % compute dp (and dq) and dc
+            dqp = inv_Hfsic * Jfsic'*(II-AA0);
+            dc = AA'*(II - Irec - J*dqp);
+
+            % This function updates the shape in an inverse compositional fashion
+            current_shape =  compute_warp_update(current_shape, dqp, cAAM.shape{ii}, cAAM.coord_frame{ii});
+        end
+        current_shape(:,1) = current_shape(:, 1) * sc(ii) ;
+        current_shape(:,2) = current_shape(:, 2) * sc(ii) ;
+    end
+
+    result = current_shape;
 end
