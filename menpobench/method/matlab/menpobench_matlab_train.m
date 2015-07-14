c7fc270982fa7550676155eccb2db5816a226124
Patrick Snape <patricksnape@gmail.com>
2015-07-14 16:20:40 +0100

    Make Matlab print more friendly messages
    Now has a little progress bar.

--- menpobench/method/matlab/menpobench_matlab_train.m
+++ menpobench/method/matlab/menpobench_matlab_train.m
@@ -3,8 +3,11 @@ function menpobench_matlab_train(method_path, training_images_path)
     menpobench_addpath_recurse(method_path);
     menpobench = menpobench_namespace();
 
+    display('Training method...');
     model = menpobench.train(training_images_path);
-    save(fullfile(method_path, 'model.mat'));
+
+    display('Saving model...');
+    save(fullfile(method_path, 'model.mat'), 'model');
 
     exit(0);
 end
