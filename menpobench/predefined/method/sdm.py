19104348ad69a734b66d77c10fd0f1ece192161c
James Booth <jabooth@gmail.com>
2015-07-27 21:32:30 +0100

    move to release version of menpofit

--- menpobench/predefined/method/sdm.py
+++ menpobench/predefined/method/sdm.py
@@ -1,10 +1,6 @@
 from menpobench.method import MenpoFitWrapper
 from menpobench.imgprocess import menpo_img_process
-from menpobench.utils import wrap_generator
-from functools import partial
-from menpofit.fitter import noisy_shape_from_bounding_box
-from menpofit.sdm import Newton, SupervisedDecentFitter
-from menpo.feature import fast_dsift
+from menpofit.sdm import SDMTrainer
 
 
 metadata = {
@@ -13,26 +9,13 @@ metadata = {
 }
 
 
-bbox_method = partial(noisy_shape_from_bounding_box, noise_percentage=0.001)
-sdm_alg = partial(Newton, alpha=0.01)
-
-
 def train(img_generator):
     # clean up the images with the standard menpo pre-processing
-    img_generator = wrap_generator(img_generator, menpo_img_process)
-    sdm = SupervisedDescentFitter(img_generator,
-                                  group='sclera',
-                                  bounding_box_group='bbox',
-                                  sd_algorithm_cls=sdm_alg,
-                                  patch_features=fast_dsift,
-                                  scales=(1, 0.5),
-                                  iterations=10,
-                                  patch_shape=(17, 17),
-                                  n_perturbations=20,
-                                  perturb_from_bounding_box=bbox_method,
-                                  verbose=False,
-                                  batch_size=None)
+    images = [menpo_img_process(img) for img in img_generator]
+    fitter = SDMTrainer(normalization_diagonal=150,
+                        downscale=1.1,
+                        n_perturbations=15).train(images, verbose=True)
 
     # return a callable that wraps the menpo fitter in order to integrate with
     # menpobench
-    return MenpoFitWrapper(sdm)
+    return MenpoFitWrapper(fitter)
