65d079ce5e9de27ebc75621aeab73bf08fef119d
Patrick Snape <patricksnape@gmail.com>
2015-08-06 17:28:06 +0100

    Ensure that the initial bbox is correct - Matlab
    It seems that the bounding box ordering was not as I thought,
    this ensures that the initial shapes are correctly oriented.

--- menpobench/method/matlab/menpobench_initial_shape_to_bbox.m
+++ menpobench/method/matlab/menpobench_initial_shape_to_bbox.m
@@ -4,10 +4,13 @@ function initial_shape = menpobench_initial_shape_to_bbox(bbox, mean_shape)
     %  Output is initial_shape, which is the mean_shape resized to fit within
     %  the bounding box.
 
-    mean_shape_bounds = reshape([min(mean_shape), max(mean_shape)], [2, 2])';
-    bbox = reshape(bbox, [2, 2])';
+    ms_bounds = [min(mean_shape), max(mean_shape)];
+    mean_shape_bbox = [ms_bounds(1), ms_bounds(2); ms_bounds(3), ms_bounds(2); 
+                       ms_bounds(3), ms_bounds(4); ms_bounds(1), ms_bounds(4)];
+    bbox = [bbox(1), bbox(2); bbox(3), bbox(2); 
+            bbox(3), bbox(4); bbox(1), bbox(4)];
 
-    [~, ~, transform] = procrustes(bbox, mean_shape_bounds);
+    [~, ~, transform] = procrustes(bbox, mean_shape_bbox);
 
     c = repmat(transform.c(1, :), [size(mean_shape, 1), 1]);
     T = transform.T;
